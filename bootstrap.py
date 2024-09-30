0a2de9b339b86dd6e19af27c8c2ae825ab9e3e2b
Your Name <you@example.com>
2024-09-30 13:13:26 -0300

    Cadastrando CSS SQLite [OK]

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,37 @@
+import os
 import re
+import sqlite3
+
+def deletar_banco_de_dados(nome_arquivo_db):
+    """Deleta um arquivo de banco de dados SQLite."""
+    if os.path.exists(nome_arquivo_db):
+        try:
+            os.remove(nome_arquivo_db)
+            print(f"Arquivo de banco de dados '{nome_arquivo_db}' deletado com sucesso.")
+        except OSError as e:
+            print(f"Erro ao deletar o arquivo: {e}")
+    else:
+        print(f"O arquivo de banco de dados '{nome_arquivo_db}' não existe.")
+
+def remover_comentarios(conteudo):
+    """Remove comentários de um bloco de texto CSS, incluindo comentários multi-linha."""
+    
+    i = 0
+    while i < len(conteudo):
+        
+        # Procurando por comentário de bloco /* ... */
+        inicio_comentario = conteudo.find("/*", i)
+        if inicio_comentario != -1:
+            fim_comentario = conteudo.find("*/", inicio_comentario + 2)
+            if fim_comentario != -1:
+                conteudo = conteudo[:inicio_comentario] + conteudo[fim_comentario + 2:]
+                #Atualiza o índice para não pular caracteres
+                i = inicio_comentario
+                continue
+        i += 1
+    
+    return conteudo
+
 def extrair_classes_propriedades(conteudo):
     """
     Extrai as classes e suas propriedades do conteúdo CSS.
@@ -12,7 +45,7 @@ def extrair_classes_propriedades(conteudo):
     classes = {}
     
     with open(arquivo_css, 'r') as arquivo:
-        conteudo = arquivo.read()
+        conteudo = remover_comentarios(arquivo.read())
 
         # Regex para encontrar classes e suas propriedades
         pattern = r'([^{]+)\s*\{([^}]*)\}'
@@ -21,7 +54,7 @@ def extrair_classes_propriedades(conteudo):
         for classe, propriedades in matches:
             # Limpa e divide múltiplas classes
             for nome_classe in classe.split(','):
-                nome_classe = nome_classe.strip().replace('}','').strip()
+                nome_classe = nome_classe.replace('}','').strip()
                 propriedades = propriedades.strip()
                 propriedades_split = [prop.strip() for prop in propriedades.split(';') if prop.strip()]
 
@@ -36,7 +69,7 @@ def extrair_classes_media(arquivo_css):
     media = {}
     try:
         with open(arquivo_css, 'r', encoding='utf-8') as arquivo:
-            conteudo = arquivo.read()
+            conteudo = remover_comentarios(arquivo.read())
     except FileNotFoundError:
         print(f"Erro: Arquivo '{arquivo_css}' não encontrado.")
         return media
@@ -84,29 +117,123 @@ def extrair_classes_media(arquivo_css):
         media[media_tipo] = classes_media
         pos = end_propriedades + 1
 
-    return media
+    return media  
 
-# Exemplo de uso
-arquivo_css = 'bootstrap.css'  # Substitua pelo caminho real
+def criar_tabelas(db):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    # Criar tabela styles_classes
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS styles_classes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        class_name TEXT,
+        media_type TEXT
+    )
+    ''')
+
+    # Criar tabela styles_classes_properties
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS styles_classes_properties (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        class_id INTEGER,
+        property_name TEXT,
+        FOREIGN KEY (class_id) REFERENCES styles_classes(id)
+    )
+    ''')
+
+    # Criar tabela styles_classes_properties_values
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS styles_classes_properties_values (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        property_id INTEGER,
+        value TEXT,
+        FOREIGN KEY (property_id) REFERENCES styles_classes_properties(id)
+    )
+    ''')
+
+     # Cria índice na tabela styles_classes pelo nome da classe
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_styles_classes_class_name ON styles_classes (class_name)")
+
+    # Cria índice na tabela styles_classes pelo tipo de mídia
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_styles_classes_media_type ON styles_classes (media_type)")
+
+    # Cria índice na tabela styles_classes_properties pelo ID da classe
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_styles_classes_properties_class_id ON styles_classes_properties (class_id)")
+
+    # Cria índice na tabela styles_classes_properties pelo nome da propriedade
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_styles_classes_properties_property_name ON styles_classes_properties (property_name)")
+
+    conn.commit()
+    conn.close()
+
+def existe_classe(classe, media_tipo):
+    cursor.execute("SELECT id FROM styles_classes WHERE class_name = ? AND media_type = ?", (classe, media_tipo))
+    return cursor.fetchone()
+
+def existe_propriedade(class_id, propriedade):
+    cursor.execute("SELECT id FROM styles_classes_properties WHERE class_id = ? AND property_name = ?", (class_id, propriedade))
+    return cursor.fetchone()
+
+def cadastra_propriedades(conn, cursor, propriedades):
+     for propriedade in propriedades:
+        prop_name, valor = map(str.strip, propriedade.split(':', 1))
+        prop_name = prop_name.strip()
+        valor = valor.strip()
+
+        if not existe_propriedade(class_id, propriedade):
+            cursor.execute("INSERT INTO styles_classes_properties (class_id, property_name) VALUES (?, ?)", (class_id, prop_name))
+            conn.commit()
+            property_id = cursor.lastrowid
+        else:
+            property_id = existe_propriedade(class_id, propriedade)[0]
+
+        cursor.execute("INSERT INTO styles_classes_properties_values (property_id, value) VALUES (?, ?)", (property_id, valor))
+        conn.commit()
+
+db = 'cms.db'
+arquivo_css = 'bootstrap.css'
+
+deletar_banco_de_dados(db)
+conn = sqlite3.connect(db)
+cursor = conn.cursor()
+
+classes = extrair_classes_propriedades(arquivo_css)
 media = extrair_classes_media(arquivo_css)
 
-print("\nClasses @media:")
+criar_tabelas(db)
+
+# Media Classes
+print('Media Classes...')
 for media_tipo, classes_media in media.items():
-    print(f"{media_tipo}:")
     for classe, propriedades in classes_media.items():
-        print(f"  {classe}:")
-        for propriedade in propriedades:
-            if propriedade:
-                print(f"    {propriedade}")
+        print(classe)
+        if not existe_classe(classe, media_tipo):
+            cursor.execute("INSERT INTO styles_classes (class_name, media_type) VALUES (?, ?)", (classe, media_tipo))
+            conn.commit()
+            class_id = cursor.lastrowid
+        else:
+            class_id = existe_classe(classe, media_tipo)[0]
 
+        cadastra_propriedades(conn, cursor, propriedades)
 
- # Encontra todas as classes normais
-classes = extrair_classes_propriedades(arquivo_css)
-
-print("Classes:")
+# Classes
+print('Classes...')
 for classe, propriedades in classes.items():
     if not classe.__contains__('@media'):
-        print(f"{classe}:")
-        for propriedade in propriedades:
-            if propriedade:  # Ignora propriedades vazias
-                print(f"    {propriedade.strip()}")
+        media_tipo = None        
+        for classe, propriedades in classes.items():
+            if not classe.__contains__('@media'):
+                print(classe)
+                if classe == 'pre':
+                    flag = False
+                if not existe_classe(classe, media_tipo):
+                    cursor.execute("INSERT INTO styles_classes (class_name, media_type) VALUES (?, ?)", (classe, media_tipo))
+                    conn.commit()
+                    class_id = cursor.lastrowid
+                else:
+                    class_id = existe_classe(classe, media_tipo)[0]
+                
+                cadastra_propriedades(conn, cursor, propriedades)
+
+conn.close()
